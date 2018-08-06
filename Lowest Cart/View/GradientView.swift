1bee4957e060742d6b6609ea401ff76201485626
Adam Gusky <gusky.adam@uwlax.edu>
2018-08-06 00:06:05 -0500

    First Commit

---
+++ Lowest Cart/View/GradientView.swift
@@ -0,0 +1,35 @@
+//
+//  GradientView.swift
+//  Lowest Cart
+//
+//  Created by Adam Gusky on 8/6/18.
+//  Copyright © 2018 Adam Gusky. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class GradientView: UIView {
+    
+    @IBInspectable var topColor: UIColor = #colorLiteral(red: 0.3872740865, green: 0.8272734284, blue: 0.06912919879, alpha: 1) {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+    
+    @IBInspectable var bottomColor: UIColor = #colorLiteral(red: 0.9370316863, green: 1, blue: 0, alpha: 1) {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+    
+    override func layoutSubviews() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [topColor.cgColor, bottomColor.cgColor]
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 1, y: 1)
+        gradientLayer.frame = self.bounds
+        self.layer.insertSublayer(gradientLayer, at: 0)
+        
+    }
+}
---
+++ Lowest Cart/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Lowest Cart
+//
+//  Created by Adam Gusky on 8/5/18.
+//  Copyright © 2018 Adam Gusky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
+
